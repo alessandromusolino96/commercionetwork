e718d7865cd0a1f9db53c3cfe5a99af1197dd0fe
Alessandro Musolino <alessandromusolino@MBPdiAlessandro.homenet.telecomitalia.it>
2022-07-26 11:11:37 +0200

    added eSignature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { Header, Segment, Statistic, Form } from 'semantic-ui-react'
+import { Header, Segment, Statistic, Form, Message } from 'semantic-ui-react';
 const qs = require("qs");
 const axios = require("axios");
 
@@ -12,13 +12,16 @@ class App extends Component {
     walletCoins: [{}, {}],
     tx_recipient: "",
     tx_amount: "",
-    tx_denom: ""
+    tx_denom: "",
+    message: "",
+    notes: "",
   };
 
   async componentDidMount() {
     this.getAuthToken();
     this.getWalletInformation();
     this.getBalance();
+    this.getSignedDocuments();
   }
 
   getAuthToken = async () => {
@@ -41,6 +44,7 @@ class App extends Component {
       this.setState({
         authToken: "Bearer " + localStorage.getItem("authToken"),
       });
+      console.log(response.data.id_token);
     });
   };
 
@@ -77,7 +81,7 @@ class App extends Component {
   };
 
   transfer = async (event) => {
-    console.log(this.state.tx_recipient, this.state.tx_amount, this.state.tx_denom);
+    // console.log(this.state.tx_recipient, this.state.tx_amount, this.state.tx_denom);
     event.preventDefault();
     const response = await axios({
       method: "post",
@@ -98,20 +102,93 @@ class App extends Component {
         "recipient": this.state.tx_recipient
       }
     }).then((response) => {
+      // console.log(response);
+      this.setState({
+        message: "Correctly sent " + response.data.amount.at(0).amount + " " + response.data.amount.at(0).denom + " to " + response.data.receiver + ". The sender token id is: " + response.data.send_token_id
+      });
+    });
+  };
+
+  signDocument = async (event) => {
+    event.preventDefault();
+    var bodyFormData = new FormData();
+    var pdfFile = document.querySelector('#document');
+    bodyFormData.append("document", pdfFile.files[0]);
+    bodyFormData.append("note", this.state.notes);
+    const response = await axios({
+      method: "post",
+      url: "https://dev-api.commercio.app/v1/sign/process",
+      headers: {
+        "accept": "application/json",
+        "Authorization": `Bearer ${localStorage.getItem("authToken")}`,
+        "content-type": "multipart/form-data",
+      },
+      data: bodyFormData,
+    }).then((response) => {
+      // console.log(response);
+    });
+  };
+
+  getSignedDocuments = async () => {
+    const response = await axios({
+      method: "get",
+      url: "https://dev-api.commercio.app/v1/sign/process",
+      headers: {
+        "accept": "application/json",
+        "Authorization": `Bearer ${localStorage.getItem("authToken")}`,
+      },
+    }).then((response) => {
       console.log(response);
+      var tbody = document.querySelector('#signed-documents');
+      var list = response.data.list;
+      list.forEach(signedDocument => {
+        // console.log(signedDocument.id);
+        var tr = document.createElement('tr');
+        var td_id = document.createElement('td');
+        var td_note = document.createElement('td');
+        var td_original_document_hash = document.createElement('td');
+        var td_original_name = document.createElement('td');
+        var td_owner_username = document.createElement('td');
+        var td_signed_document_hash = document.createElement('td');
+        var td_signer_username = document.createElement('td');
+        var td_signature_time = document.createElement('td');
+        var td_status = document.createElement('td');
+        var td_tx_process_id = document.createElement('td');
+        td_id.innerHTML = signedDocument.id;
+        td_note.innerHTML = signedDocument.note;
+        td_original_document_hash.innerHTML = signedDocument.original_document.hash;
+        td_original_name.innerHTML = signedDocument.original_document.original_name;
+        td_owner_username.innerHTML = signedDocument.owner_username;
+        td_signed_document_hash.innerHTML = signedDocument.signed_document.hash;
+        td_signer_username.innerHTML = signedDocument.signer_username;
+        td_signature_time.innerHTML = signedDocument.sign_end_at;
+        td_status.innerHTML = signedDocument.status;
+        td_tx_process_id.innerHTML = signedDocument.tx_process_id;
+        tr.appendChild(td_id);
+        tr.appendChild(td_note);
+        tr.appendChild(td_original_document_hash);
+        tr.appendChild(td_original_name);
+        tr.appendChild(td_owner_username);
+        tr.appendChild(td_signed_document_hash);
+        tr.appendChild(td_signer_username);
+        tr.appendChild(td_signature_time);
+        tr.appendChild(td_status);
+        tr.appendChild(td_tx_process_id);
+        tbody.appendChild(tr);
+      });
     });
   };
 
   render() {
     return (
       <div>
-      <link
-        async
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
-      />
+        <link
+          async
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+        />
 
-      <Segment clearing>
+        <Segment clearing>
           <Header as='h2' floated='right'>
             Account Number: {this.state.accountNumber}
           </Header>
@@ -132,6 +209,10 @@ class App extends Component {
         </Statistic.Group>
 
         <hr />
+
+        <h1>
+          Transfer money
+        </h1>
 
         <Form onSubmit={this.transfer}>
           <Form.Group widths='equal'>
@@ -159,6 +240,57 @@ class App extends Component {
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
+
+        <Message>
+          <Message.Header>Message status</Message.Header>
+          <p>
+            {this.state.message}
+          </p>
+        </Message>
+
+        <hr />
+        <br />
+
+        <div>
+          <h1>Sign a document</h1>
+
+          <Form onSubmit={this.signDocument}>
+            <Form.Group widths='equal'>
+              <input type="file" name="document" id="document" accept="application/pdf" />
+              <Form.Input
+                fluid
+                label='note'
+                placeholder='note'
+                value={this.state.notes}
+                onChange={event => this.setState({ notes: event.target.value })}
+              />
+            </Form.Group>
+            <Form.Button>Submit</Form.Button>
+          </Form>
+        </div>
+
+        <br />
+
+        <div>
+          <h1>Signed Documents</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Note</th>
+                <th>Original document hash</th>
+                <th>Original name</th>
+                <th>Owner username</th>
+                <th>Signed document hash</th>
+                <th>Signer username</th>
+                <th>Signature time</th>
+                <th>Status</th>
+                <th>Tx process id</th>
+              </tr>
+            </thead>
+            <tbody id="signed-documents" />
+          </table>
+        </div>
 
       </div>
     );
